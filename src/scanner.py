31f9483ac5803cf1a523d804274368830836e27d
Pleskr011 <fersyspl3@proton.me>
2026-02-06 19:40:19 -0400

    Changing from subprocess to manager from bandit.core

--- src/scanner.py
+++ src/scanner.py
@@ -1,26 +1,40 @@
 import argparse
 import subprocess
 import json
+from bandit.core import manager
 
 from ai_advisor import analyze_with_ai
+
+def run_bandit(target_path):
+    bandit_mgr = manager.BanditManager()
+    bandit_mgr.discover_files([target_path])
+    bandit_mgr.run_tests()
+
+    results = bandit_mgr.get_issue_list()
+    return results
 
 def run_security_scan(target_path, ai_platform):
     print(f"🚀 Iniciando escaneo de seguridad en {target_path}...")
     
-    # Ejecutamos Bandit sobre la carpeta src/
-    result = subprocess.run(
-        ["bandit", "-r", target_path, "-f", "json"], 
-        capture_output=True, 
-        text=True,
-        shell=False
-    )
+    # Se ejecuta Bandit sobre la carpeta base
+    # Se detectó riesgo de shell injection, por lo que se evita usar shell=True
+    # Bandit sigue detectando vulnerabilidades, asi que se opta por usar bandit directamente
+    
+    #result = subprocess.run(
+    #    ["bandit", "-r", target_path, "-f", "json"], 
+    #    capture_output=True, 
+    #    text=True,
+    #    shell=False
+    #)
+    
+    result = run_bandit(target_path)
     
     # Bandit devuelve código 1 si encuentra vulnerabilidades
     scan_data = json.loads(result.stdout)
     
     if scan_data['results']:
         print(f"⚠️ Se encontraron {len(scan_data['results'])} vulnerabilidades.")
-        # Enviamos los hallazgos a la IA para obtener sugerencias
+        # Se envía lo detectado a la IA para obtener sugerencias
         report = analyze_with_ai(scan_data, ai_platform)
         print("\n--- REPORTE DE IA ---\n")
         print(report)
